b69dd974d19e28ff5273d8caf3bd8e31879476bf
Rohan <rhnmht30@gmail.com>
2021-01-28 21:27:02 +0530

    fix: format to import next/image

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import MyImage from "../components/MyImage";
+import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
