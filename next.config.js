e44918e5a6c80efc6f126f854f1200657bfc2e18
Rohan <rhnmht30@gmail.com>
2021-01-28 21:25:45 +0530

    feat: add cloudinary as loader in next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   // Target must be serverless
   target: "serverless",
+  images: {
+    loader: "cloudinary",
+    path: "https://res.cloudinary.com/rhnmht30/image/upload/",
+  },
 };
-
